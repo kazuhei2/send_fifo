7de5d05c23b073cc313604c478963bcddb63c8a8
Kazuhei Hamada <kazuhei.git@gmail.com>
2020-03-24 23:19:14 +0900

    Write some data to fifo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate nix;
+extern crate tempfile;
+
+use nix::fcntl;
+use nix::sys::stat;
+use nix::unistd;
+use std::thread::sleep;
+use std::time;
+use tempfile::tempdir;
+
 fn main() {
-    println!("Hello, world!");
+    let tmp_dir = tempdir().unwrap();
+    let fifo_path = tmp_dir.path().join("foo.pipe");
+
+    match unistd::mkfifo(&fifo_path, stat::Mode::S_IRWXU) {
+        Ok(_) => println!("created {:?}", fifo_path),
+        Err(err) => println!("Error creating fifo: {}", err),
+    }
+
+    let wait_ms = time::Duration::from_millis(1000);
+    let fd = fcntl::open(&fifo_path, fcntl::OFlag::O_WRONLY, stat::Mode::S_IRWXU).unwrap();
+    let data = "hello".as_bytes();
+    loop {
+        unistd::write(fd, &data).unwrap();
+        sleep(wait_ms);
+    }
 }
