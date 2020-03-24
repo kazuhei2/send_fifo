8f40de1d413567be3270c717494cfdab11148969
Kazuhei Hamada <kazuhei.git@gmail.com>
2020-03-25 01:01:38 +0900

    mkfifo and write example works

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 extern crate nix;
 extern crate tempfile;
 
-use nix::fcntl;
 use nix::sys::stat;
 use nix::unistd;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
 use std::thread::sleep;
 use std::time;
-use tempfile::tempdir;
 
 fn main() {
-    let tmp_dir = tempdir().unwrap();
-    let fifo_path = tmp_dir.path().join("foo.pipe");
+    let fifo_path = Path::new("/tmp/foo.pipe");
 
-    match unistd::mkfifo(&fifo_path, stat::Mode::S_IRWXU) {
+    match unistd::mkfifo(fifo_path, stat::Mode::S_IRWXU) {
         Ok(_) => println!("created {:?}", fifo_path),
         Err(err) => println!("Error creating fifo: {}", err),
     }
 
-    let wait_ms = time::Duration::from_millis(1000);
-    let fd = fcntl::open(&fifo_path, fcntl::OFlag::O_WRONLY, stat::Mode::S_IRWXU).unwrap();
-    let data = "hello".as_bytes();
-    loop {
-        unistd::write(fd, &data).unwrap();
-        sleep(wait_ms);
-    }
+    let mut file = OpenOptions::new().write(true).open(&fifo_path).unwrap();
+    file.write_all(b"Hello, world!").unwrap();
+    println!("file.write_all() done");
+    sleep(time::Duration::from_secs(3));
 }
